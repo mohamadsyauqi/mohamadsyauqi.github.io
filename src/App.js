0a85a96f49a74e208c947ed2fde516d9c3ce4d8e
Mohamad Syauqi Mazuki <mohamadsyauqimazuki@gmail.com>
2021-03-25 18:09:44 +0800

    Update message

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello Cuda</h1>
+        <h1>Hello Cuda Nak Kopi Satu!</h1>
         <p>:)</p>
       </header>
     </div>
